229572ee99b1ee19787832f587f4048621651929
sami <s.ben1bedelhafidh@gmail.com>
2024-12-24 17:57:16 +0100

    'second'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone:true,
   imports: [RouterOutlet,
     HomeComponent,
     AboutComponent,
-    NavbarComponent
+    NavbarComponent,
+    FormsModule,
 
 
   ],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { NgModule } from '@angular/core';
+
 @Component({
   selector: 'app-home',
   imports: [],
@@ -11,4 +13,26 @@ export class HomeComponent {
 firstName:"sami",
 lastName:"abedelhafidh",
 age:"23"}
+linkImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg8184XmlGy4JPXNX4NOOD-KDX1I8u4jx7Xw&s"
+
+number=1
+titleStyle="color=red"
+inc(){
+  this.number++
+}
+dec(){
+  this.number--
+}
+red(){
+  this.titleStyle="color:red;font-size:40px;"
+}
+green(){
+  this.titleStyle="color:green;font-size:20px;"
+}
+
+
+verif=true
+
+dataArray=["tunisia","morroco","france","kelibia"]
+
 }
